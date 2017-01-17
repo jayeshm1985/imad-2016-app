b6fc07e5c9dfc47553dd5d7618458d65c4814d87
Jayesh M <jayeshm1985@gmail.com>
2017-01-17 11:57:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,6 +27,9 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/jayesh1.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'jayesh1.jpg'));
 });
+app.get('/ui/jayeshlatestnew.doc', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'jayeshlatestnew.doc'));
+});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
